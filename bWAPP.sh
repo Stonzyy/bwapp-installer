a25a39dadd0748cfda5852f04cbcd9652d54f07d
Stonzyy <33039121+Stonzyy@users.noreply.github.com>
2021-07-18 03:24:59 -0400

    Update bWAPP.sh

--- bWAPP.sh
+++ bWAPP.sh
@@ -1,3 +1,4 @@
+echo "Installation bWAPP Start ...."
 cd ~/Downloads
 unzip bWAPP_latest.zip
 sudo mv bWAPP /var/www/html/
@@ -5,4 +6,8 @@ cd /var/www/html/bWAPP
 sudo chmod 777 */
 sudo service apache2 start
 sudo service mysql start
+echo "bWAPP Ready To Start"
+
 open http://localhost/bWAPP/install.php
+
+
